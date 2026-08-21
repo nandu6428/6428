95d0737c825a4d723b2cd8af8ec57df6e52fe771
nandu6428 <nanducv2011@gmail.com>
2026-08-21 15:58:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ header {
     top: 0;
     left: 0;
     width: 100%;
-    padding: 20px 40px;
+    padding: 40px 80px;
     background: rgba(15, 15, 15, 0.85);
     backdrop-filter: blur(10px);
     z-index: 1000;
